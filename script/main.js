9a2b36f40691bae812de3c233f38db73f61f8bf8
Heidi <heiding1127@gmail.com>
2020-05-11 11:46:47 -0700

    review

--- script/main.js
+++ script/main.js
@@ -1,16 +1,10 @@
-$(window).scroll(function(e) {
-  frames = 17;
-  step = ($("body").height() - $(window).height()) / frames;
-  scrollStep = parseInt($(window).scrollTop() / step);
-  maskPosition = 100 / frames * scrollStep;
-  $("#cover").css({
-    "mask-position": maskPosition + "% 50%",
-    "-webkit-mask-position": maskPosition + "% 50%"
-  });
-});
-
-/* Just for CodePen Thumbnail */
-$("html, body").animate({ scrollTop: $(document).height() - $(window).height() }, 1200).queue(function(next) {
-  $("html, body").animate({ scrollTop: 0 }, 1200);
-  next();
-});
+<script>
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+</script>
